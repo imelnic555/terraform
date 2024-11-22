42f876a4f59bb6015d9472e306a4e290c96a6d61
imelnic <Performance10$@>
2024-11-22 10:21:34 +0200

    Added terraform file

--- main.tf
+++ main.tf
@@ -1,11 +1,12 @@
+# Configure the AWS provider
+
+
 provider "aws" {
-  region = "us-east-1"
+  region = "us-east-1"
 }
-
-
 
 # Create an EC2 instance
 resource "aws_instance" "example" {
-  ami           = "ami-0866a3c8686eaeeba"
-  instance_type = "t2.micro"
+  ami           = "ami-0866a3c8686eaeeba"
+  instance_type = "t2.micro"
 }
